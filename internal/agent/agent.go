14edc91c5f3e8cd1ea650301cef9edda9c3e58a9
Anirudh Konidala <kidskodingclub@gmail.com>
2026-01-02 23:14:00 -0600

    added skeleton and dummy data for testing

--- internal/agent/agent.go
+++ internal/agent/agent.go
@@ -1,9 +1,27 @@
 package agent
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func StartAgent() {
-	fmt.Println("agent loop starts here")
+	memory := &SessionMemory{
+		LastTracks: []string{},
+		SkipHistory: make(map[string]bool),
+		EnergyHistory: []float64{},
+		CurrentMode: "medium",
+	}
 
-	// TODO: Implement observation -> memory -> reasoning -> action loop
+	for i := 0; i < 3; i++ {
+		fmt.Println("agent decides next track...")
+		DecideNextTrack(memory)
+		time.Sleep(1 * time.Second)
+	}
+}
+
+func DecideNextTrack(memory *SessionMemory) {
+	nextTrack := "track_" + fmt.Sprint(len(memory.LastTracks)+1)
+    memory.LastTracks = append(memory.LastTracks, nextTrack)
+    fmt.Println("Next track selected:", nextTrack)
 }
